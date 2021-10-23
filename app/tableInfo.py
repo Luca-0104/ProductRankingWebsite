b5c16e7e47ecad6f8af4c295a7b64700f9c505f1
Geng <tianyi.geng@ucdconnect.ie>
2021-10-23 22:30:05 +0800

    Add product picture, fill product data in different categories.

--- app/tableInfo.py
+++ app/tableInfo.py
@@ -3,7 +3,101 @@ For inserting the information into the database
 Those information should be stored in this file
 """
 
+#
 user_list = [
     ['Customer1@163.com', 'Customer1', '123', 1],
     ['Retailer1@163.com', 'Retailer1', '123', 2]
 ]
+
+#
+product_picture_list = [
+    # [name1, name2, ...] (at least 2, at most 5)
+    # all in png, without spacing
+    ['91u24xvKpxL._AC_SX679_.jpg', '914XpdnqSrL._AC_SL1500_.jpg'],  # pic of product 0
+    ['81vPJY1agyL._SL1500_.jpg', '81jZWKisi+L._SL1500_.jpg', '71n54nZiDuL._SL1500_.jpg'],  # pic of product 1
+    ['6166TvCN33S._AC_SL1500_.jpg', '51W9Zo0aHxS._AC_SL1500_.jpg', '61G-xklXq-S._AC_SL1500_.jpg'],
+    ['91TKOKJe2zL._SL1500_.jpg', '91XslFohKxL._SL1500_.jpg'],
+    ['9150Gx-BrKS._AC_SL1500_.jpg', '915zTdcqILS._AC_SL1500_.jpg'],
+    ['61-ggLv1AuL._AC_UX466_.jpg', '61GL637HSNL._AC_UX466_.jpg', '61eQ776sLuL._AC_UX342_.jpg'],
+    ['finishes_1_sierra_blue__2bovafkl4yaa_large.jpg', 'iphone-13-pro-blue-select.png'],
+    [],
+    [],
+    [],
+    [],
+    [],
+
+]
+
+"""
+Categories:
+sports
+foods
+clothes
+digital
+books
+makeup
+luxury
+daily
+art
+home
+instruments
+others
+"""
+
+#
+product_category_list = [
+    # [cate1, cate2, cate3, ...] (must have at least 1 single category tag)
+    ['sports'],
+    ['foods'],
+    ['sports'],
+    ['foods'],
+    ['clothes'],
+    ['clothes'],
+    ['digital'],
+    [],
+    [],
+    [],
+
+
+]
+
+#
+product_list = [
+    # name, description, price,
+    ['Spalding basketball', 'Youth size and weight: size 5, 27.5 Performance composite cover\
+Deep channel design for superior control\
+Butyl rubber bladder for air retention\
+Designed for indoor play only', 290.84],
+    ['Lindt Gourmet Chocolate Truffle Gift Box', 'Share the passion of delectable chocolate or have a moment of indulgence alone\
+Perfect for indulging with at home or as a surprise gift\
+Made by Lindt Master Chocolatiers with premium ingredients from world-renowned regions\
+The 14.7 oz. Box of gourmet truffles is an ideal gift for any occasion', 178.773],
+    ['BURTON Custom Flying V Snowboard', "Directional Shape is the classic snowboard shape, designed to be ridden with a slightly longer nose than tail to concentrate pop in the tail while providing plenty of float, flow, and control to rip any terrain or condition\
+Flying V Bend features rocker zones between and outside your feet for enhanced playfulness, and float and camber zones underneath your feet that focus edge control for crisp snap, added pop, and powerful turns\
+Twin Flex is perfectly symmetrical from tip to tail for a balanced ride that's equally versatile regular or switch\
+Super Fly II 700G Core uses stronger and lighter wood to provide pop and strength while reducing overall weight\
+Dualzone EGD engineered wood grain is positioned along the toe and heel edges on two continuous zones perpendicular to the core for more edge-hold, response, and strength", 4021.54],
+    ["Lay's Potato Chip Variety Pack", "Variety pack of Lay's potato chips favorites with classic flavors in one convenient package\
+With 4 different varieties, there's sure to be something everyone will love\
+40 count pack featuring 10 of each of these Lay's potato chips favorites in 1 oz bags - Classic, Barbecue, Sour Cream and Onion, and Salt and Vinegar\
+These much loved treats are fun to enjoy at lunch, as an after-school snack, or party refreshment\
+Easy to carry, easy to store, and easy to pack\
+Our snacks have a short shelf life (60-90 days) so most of our packages only show the month & day of expiration. For optimum flavor and freshness, we recommend the snack be consumed by the date on the package.", 57.39],
+    ["Legendary Whitetails Men's Buck Camp Flannel Shirt", "100% Cotton PERFECT WEIGHT: Weighing in at 5.1 ounces, the Buck Camp Flannel Shirt is the perfect weight to be worn alone or layered, indoors or outside; you're gonna love this comfortable brushed cotton flannel shirt\
+DURABLE QUALITY: Our signature corduroy lined collar and cuffs not only look cool, but add stablility. which means they will hold their shape so you don't have to iron", 190.88],
+    ['Bbonlinedress Women Short 1950s Retro Vintage Cocktail Party Swing Dresses' ,'98% Cotton, 2% Spandex, Zipper closure, Machine Wash\
+Size: Please use Size Chart Image Left / Size Info in Product Description (XS-5XL) to choose a suitable size. Our size is different from standard size, thanks.\
+Features: Vintage Black Dress, A-line Style, Sweetheart Neckline, Cap Sleeves, Back Zipper Closure, Classic Vintage Party Dress.\
+Material: 98% Cotton & 2% Spandex.\
+Great for Wedding Party, Bridesmaid, Cocktail Party, Birthday, Work, Holiday, Tea Party, Dinner, Anniversary, Theme Party and Daily Casual.\
+Garment Care: Hand wash cold hang dry recommended, machine washable. Do not wring or twist, low iron to remove wrinkles if necessary.', 245.89],
+    ['iPhone 13 Pro', 'A dramatically more powerful camera system. A display so responsive, every interaction feels new again. The world’s fastest smartphone chip. Exceptional durability. And a huge leap in battery life.', 5999],
+    [],
+    [],
+    [],
+    [],
+    [],
+
+
+]
+
